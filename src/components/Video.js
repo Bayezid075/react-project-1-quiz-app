c9b5c782bc07a2221a34924a285aa1c0c579c3ea
Bayezid075 <mrbayezidhoshen@gmail.com>
2022-06-26 17:59:11 +0600

    dynamic video ,title, noq

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import image from "../assets/images/3.jpg";
 import classes from "../styles/Video.module.css";
-function Video() {
+function Video({ title, id, noq }) {
   return (
     <div className={classes.video}>
-      <img src={image} alt="" />
-      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      <img
+        src={`http://img.youtube.com/vi/${id}/maxresdefault.jpg`}
+        alt={title}
+      />
+      <p>{title}</p>
       <div className={classes.qmeta}>
-        <p>10 Questions</p>
-        <p>Score : Not taken yet</p>
+        <p>{noq} Questions</p>
+        <p>Total Score : {noq * 5} </p>
       </div>
     </div>
   );
